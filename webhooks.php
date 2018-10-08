a315f62a5017fdaa6f3571cee7bf502ff09b4ab2
bankben2005 <bankben2005>
2018-10-08 19:05:56 +0700

    dsadsa

--- webhooks.php
+++ webhooks.php
@@ -6,7 +6,7 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 $access_token = 'Ehx/Hu1wH3uEtKDQyaDH5QAFN33t4H2M7kFfxAP7+2H5yytGbjv44Jn08ZWkNAQ08VITfnKwSa3GWbX4Fw4/IKv3pdrjik3Kp3TdGwlgFlDUWrmOxjb6hCpKzxTs3UYfljgUP3HwQgD8+56jIORaVgdB04t89/1O/w1cDnyilFU=';
  
 // Get POST body content
-$content = file_get_contents('php://input');
+$content = file_get_contents('php://input',FILE_USE_INCLUDE_PATH);
 
 
 // Parse JSON
