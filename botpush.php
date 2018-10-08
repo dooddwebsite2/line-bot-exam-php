3e604ca7d642359334603d4bdc5f04fe44c3604c
bankben2005 <bankben2005>
2018-10-08 18:48:59 +0700

    feautre

--- botpush.php
+++ botpush.php
@@ -6,7 +6,7 @@ require "vendor/autoload.php";
 
 $access_token = 'Ehx/Hu1wH3uEtKDQyaDH5QAFN33t4H2M7kFfxAP7+2H5yytGbjv44Jn08ZWkNAQ08VITfnKwSa3GWbX4Fw4/IKv3pdrjik3Kp3TdGwlgFlDUWrmOxjb6hCpKzxTs3UYfljgUP3HwQgD8+56jIORaVgdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = 'c6d29fd704687b143ac24f680b7cc2f1';
+$channelSecret = '8b36b547e2f6a0be2c62557685fdaddb';
 
 $pushID = 'U7605b7a20c999dd727af0c68561b6e5c';
 
--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 
 $access_token = 'Ehx/Hu1wH3uEtKDQyaDH5QAFN33t4H2M7kFfxAP7+2H5yytGbjv44Jn08ZWkNAQ08VITfnKwSa3GWbX4Fw4/IKv3pdrjik3Kp3TdGwlgFlDUWrmOxjb6hCpKzxTs3UYfljgUP3HwQgD8+56jIORaVgdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'U7605b7a20c999dd727af0c68561b6e5c';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
